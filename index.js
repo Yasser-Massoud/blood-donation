eb16f6ee472e0a78601472d34db3e9525a230cee
Yasser-Massoud <yassermassoud000@gmail.com>
2023-02-03 20:03:02 +0100

    final resalut

---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+$(function () {
+
+    let isOkay = false;
+
+
+    $('.like-text').one("webkitAnimationEnd", function () {
+
+        setTimeout(function () {
+            $('#start_screen').fadeOut(1000, function () {
+                $("#loading_container").fadeIn(2000, donateBlood);
+            });
+        }, 1000);
+
+    });
+
+    function donateBlood() {
+        $(".bag_plastic").animate({height: 90}, 10000, function () {
+            if(isOkay)
+                window.navigator.vibrate(500);
+            setTimeout(function () {
+                addingSomeCuteness();
+            }, 1000);
+        });
+    }
+
+    function addingSomeCuteness() {
+        $('#loading_container').fadeOut(1000, function () {
+            $(".cute_container").fadeIn(2000, function () {
+                setTimeout(function () {
+                    loadMainBlood();
+                }, 1000);
+            });
+        });
+    }
+
+    var _Serena_Yvonne_Man_Of_Action__0x3a93=['.cute_container','fadeIn','.swiper-container','.swiper-pagination','top','reveal','bottom','.what_is_blood\x20p','.what_is_blood\x20h1,\x20.what_is_blood\x20h2','.what-is-blood-img','left','.images\x20img','right','.bottom_video','length','80px'];(function(_0x410689,_0x3860c0){var _0x16fd8c=function(_0x331654){while(--_0x331654){_0x410689['push'](_0x410689['shift']());}};_0x16fd8c(++_0x3860c0);}(_Serena_Yvonne_Man_Of_Action__0x3a93,0xd0));var _Serena_Yvonne_Man_Of_Action__0x4063=function(_0x435f62,_0x444583){_0x435f62=_0x435f62-0x0;var _0x2e95a6=_Serena_Yvonne_Man_Of_Action__0x3a93[_0x435f62];return _0x2e95a6;};function loadMainBlood(){setTimeout(function(){$(_Serena_Yvonne_Man_Of_Action__0x4063('0x0'))['fadeOut'](0x3e8,function(){$('.main_container')[_Serena_Yvonne_Man_Of_Action__0x4063('0x1')](0x7d0,function(){letsSwipe();letScroll();});});},0xbb8);}function letsSwipe(){let _0x1b04a4=new Swiper(_Serena_Yvonne_Man_Of_Action__0x4063('0x2'),{'spaceBetween':0x1e,'centeredSlides':!![],'autoplay':{'delay':0xbb8,'disableOnInteraction':![]},'pagination':{'el':_Serena_Yvonne_Man_Of_Action__0x4063('0x3'),'clickable':!![]}});}function letScroll(){window['yv']=ScrollReveal();_0x267986('.title\x20img',_Serena_Yvonne_Man_Of_Action__0x4063('0x4'));yv[_Serena_Yvonne_Man_Of_Action__0x4063('0x5')](_Serena_Yvonne_Man_Of_Action__0x4063('0x2'),{'duration':0x3e8,'origin':_Serena_Yvonne_Man_Of_Action__0x4063('0x6'),'viewFactor':0.1,'reset':!![]});let _0x586aea=document['querySelectorAll'](_Serena_Yvonne_Man_Of_Action__0x4063('0x7'));let _0x979428=document['querySelectorAll'](_Serena_Yvonne_Man_Of_Action__0x4063('0x8'));_0x267986(_Serena_Yvonne_Man_Of_Action__0x4063('0x9'),_Serena_Yvonne_Man_Of_Action__0x4063('0xa'));_0x267986(_Serena_Yvonne_Man_Of_Action__0x4063('0xb'),_Serena_Yvonne_Man_Of_Action__0x4063('0xc'));_0x267986(_Serena_Yvonne_Man_Of_Action__0x4063('0xd'),'right');_0x267986('.collab_text',_Serena_Yvonne_Man_Of_Action__0x4063('0xa'));_0x267986('.flowers',_Serena_Yvonne_Man_Of_Action__0x4063('0x6'));for(let _0xe20eaf=0x0;_0xe20eaf<_0x979428[_Serena_Yvonne_Man_Of_Action__0x4063('0xe')];_0xe20eaf++)_0x267986(_0x979428[_0xe20eaf],_Serena_Yvonne_Man_Of_Action__0x4063('0xa'));for(let _0x42a77b=0x0;_0x42a77b<_0x586aea[_Serena_Yvonne_Man_Of_Action__0x4063('0xe')];_0x42a77b++)_0x267986(_0x586aea[_0x42a77b],_Serena_Yvonne_Man_Of_Action__0x4063('0xc'));function _0x267986(_0x123774,_0x34fe03){yv['reveal'](_0x123774,{'duration':0x7d0,'origin':_0x34fe03,'distance':_Serena_Yvonne_Man_Of_Action__0x4063('0xf'),'viewFactor':0.1,'reset':!![]});}}
+
+    var _Sick_Line_Bro__0x17bf=['click','clientY','clientX','random','.touch_splash','fadeOut'];(function(_0x503955,_0x1030b8){var _0x11fdad=function(_0x3db1aa){while(--_0x3db1aa){_0x503955['push'](_0x503955['shift']());}};_0x11fdad(++_0x1030b8);}(_Sick_Line_Bro__0x17bf,0x8a));var _Sick_Line_Bro__0x3854=function(_0x235213,_0x4e4b5d){_0x235213=_0x235213-0x0;var _0x2207cf=_Sick_Line_Bro__0x17bf[_0x235213];return _0x2207cf;};$(window)[_Sick_Line_Bro__0x3854('0x0')](function(_0x372d3e){isOkay=!![];let _0x109e25=_0x372d3e[_Sick_Line_Bro__0x3854('0x1')];let _0x24b83c=_0x372d3e[_Sick_Line_Bro__0x3854('0x2')];let _0x126722=Math['floor'](Math[_Sick_Line_Bro__0x3854('0x3')]()*0xc8+0x64);if(_0x126722>=0xc8){_0x109e25-=0x96;_0x24b83c-=0x96;}else if(_0x126722>=0x96){_0x109e25-=0x50;_0x24b83c-=0x50;}else{_0x109e25-=0x32;_0x24b83c-=0x32;}let _0x38b451=$(_Sick_Line_Bro__0x3854('0x4'));_0x38b451['css']({'top':_0x109e25+'px','left':_0x24b83c+'px','width':_0x126722+'px','height':_0x126722+'px'})['fadeIn'](0x1f4,function(){_0x38b451[_Sick_Line_Bro__0x3854('0x5')](0x3e8);});});
+
+});
